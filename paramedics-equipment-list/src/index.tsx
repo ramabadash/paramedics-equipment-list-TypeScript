2178a2bb8526bf1696e3e1ea1bda4f33e73bc61d
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-12-26 20:21:29 +0200

    # Changs components folder name

--- paramedics-equipment-list/src/index.tsx
+++ paramedics-equipment-list/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
 import { BrowserRouter as Router, HashRouter } from 'react-router-dom';
-import App from './componens/App';
+import App from './components/App';
 
 ReactDOM.render(
   <React.StrictMode>
