0ff9817dd78035aa44a4fd7800059cd10bdcd2b5
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-12-26 18:22:39 +0200

    # Provide store to index.ts and display app

--- paramedics-equipment-list/src/index.tsx
+++ paramedics-equipment-list/src/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { store } from './app/store';
+import { BrowserRouter as Router, HashRouter } from 'react-router-dom';
+import App from './componens/App';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
-    <h1>Hello world!</h1>
-    {/* </Provider> */}
+    <Provider store={store}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
