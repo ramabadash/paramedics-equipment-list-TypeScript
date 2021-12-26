c81610c7e374075576af2b0eeaec960908b09b83
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-12-26 17:08:24 +0200

    # Delete files

--- paramedics-equipment-list/src/index.tsx
+++ paramedics-equipment-list/src/index.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { store } from './app/store';
 import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    {/* <Provider store={store}> */}
+    <h1>Hello world!</h1>
+    {/* </Provider> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
