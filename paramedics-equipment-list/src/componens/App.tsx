6f89257e20d4f004333b69e65fcf93cf2366fe9e
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-12-26 20:03:56 +0200

    # Copy and adjust to typeScript the InventoryList component

--- paramedics-equipment-list/src/componens/App.tsx
+++ paramedics-equipment-list/src/componens/App.tsx
@@ -1,23 +1,36 @@
 import React from 'react';
 import { useAppSelector } from '../app/hooks';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+// Components
 import EntryForm from './EntryForm';
+import InventoryList from './InventoryList';
 // Style
 import '../styles/App.css';
 
 function App() {
+  /***** FUNCTIONS *****/
+  const navigate = useNavigate();
+
+  // Navigate to entry form if not logged
+  const isLogged = (loggedAnswer: boolean) => {
+    if (!loggedAnswer) {
+      navigate('/');
+    }
+  };
   const worker = useAppSelector(state => state.worker);
   console.log({ worker });
+  const equipment = useAppSelector(state => state.equipment);
+  console.log({ equipment });
 
   return (
     <div>
       <Routes>
         <Route path='/' element={<EntryForm />} />
-        {/* <Route
+        <Route
           path='/inventory'
           element={<InventoryList isLogged={isLogged} />}
         />
-        <Route path='/end' element={<End isLogged={isLogged} />} />
+        {/* <Route path='/end' element={<End isLogged={isLogged} />} />
         <Route path='/missing' element={<MissingItems isLogged={isLogged} />} /> */}
       </Routes>
     </div>
---
+++ paramedics-equipment-list/src/componens/InventoryList.tsx
@@ -0,0 +1,82 @@
+import React, { useState, useEffect } from 'react';
+import { useAppSelector, useAppDispatch } from '../app/hooks';
+import { useNavigate } from 'react-router-dom';
+// Actions
+import { submitEquipmentForm } from '../reducers/equipmentReducer';
+// Components
+import AddItemForm from './AddItemForm';
+import ItemRow from './ItemRow';
+// Types
+import { ShiftItem } from '../types/types';
+// Style
+import '../styles/InventoryList.css';
+
+function InventoryList({
+  isLogged,
+}: {
+  isLogged: (loggedAnswer: boolean) => void;
+}) {
+  /***** STATES *****/
+  // Get full list from state
+  const fullEquipmentList = useAppSelector(
+    ({ equipment }) => equipment.requiredEquipmentList
+  );
+  // Get is logged
+  const logged = useAppSelector(({ worker }) => worker.logged);
+
+  // State to all the missing items [{name: "equipment", missing: "80"}, ...]
+  const [missingItems, setMissingItems] = useState<ShiftItem[]>([]);
+
+  /***** EFFECT *****/
+  useEffect(() => {
+    isLogged(logged);
+  });
+
+  /***** FUNCTIONS *****/
+  const navigate = useNavigate();
+
+  const dispatch = useAppDispatch();
+
+  const handleSubmit = () => {
+    dispatch(submitEquipmentForm({ data: missingItems }));
+    navigate('/end');
+  };
+
+  return (
+    <div className='inventory-list-container'>
+      <h1>Welcome to the shift!</h1>
+
+      <h2>Inventory List</h2>
+
+      <table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Item's name</th>
+            <th>Full quantity</th>
+            <th>Current quantity</th>
+            <th>Missing</th>
+          </tr>
+        </thead>
+        <tbody>
+          {fullEquipmentList.map((item, i) => {
+            return (
+              <ItemRow
+                item={item}
+                i={i}
+                key={i}
+                setMissingItems={setMissingItems}
+              />
+            );
+          })}
+        </tbody>
+        <AddItemForm numOfEquipment={fullEquipmentList.length} />
+      </table>
+      <button onClick={handleSubmit} className='submit-btn'>
+        Done <i className='far fa-check-circle'></i>
+      </button>
+    </div>
+  );
+}
+
+export default InventoryList;
